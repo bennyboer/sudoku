5cc8b1fab71cda694a957e8b4f7f658d8cb60bec
Benjamin Eder <barny.eder@gmail.com>
2019-03-24 22:13:12 +0100

    #7 | Backtracking solver example to fill a random Sudoku board

--- solver/backtracking/backtracking_solver.go
+++ solver/backtracking/backtracking_solver.go
@@ -3,6 +3,8 @@ package backtracking
 import (
 	"github.com/ob-algdatii-ss19/leistungsnachweis-sudo/model"
 	"github.com/ob-algdatii-ss19/leistungsnachweis-sudo/solver/backtracking/strategy"
+	"math/rand"
+	"time"
 )
 
 // Solver using the backtracking algorithm.
@@ -20,15 +22,24 @@ func (s *Solver) Solve(sudoku *model.Sudoku) (bool, error) {
 
 	emptyCells := *cellChooser.Get(sudoku)
 
+	rand.Seed(time.Now().UnixNano())         // Seed random number generator
+	values := make([][]int, len(emptyCells)) // What values are left to be tried for the cell
+	for i := 0; i < len(emptyCells); i++ {
+		values[i] = rand.Perm(model.SudokuSize)
+	}
+
 	for i := 0; i < len(emptyCells); {
 		cell := emptyCells[i]
 
 		success := false
-		for value := cell.Value() + 1; value <= 9; value++ {
+		for a := 0; a < len(values[i]); a++ {
+			value := values[i][a] + 1
+
 			cell.SetValue(value)
 
 			if !cell.HasCollision() {
 				success = true
+				values[i] = values[i][a+1:] // Update values left to try for this cell
 				break
 			}
 		}
@@ -38,6 +49,7 @@ func (s *Solver) Solve(sudoku *model.Sudoku) (bool, error) {
 		} else {
 			// "Backtrack": Set value again to 0 and try again with last cell
 			cell.SetValue(0)
+			values[i] = rand.Perm(model.SudokuSize)
 
 			// First and foremost, check if backtracking is possible
 			if i > 0 {
