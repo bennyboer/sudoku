37d8375cf72fc330f484ee1652a7072c756f5ad0
Benjamin Eder <barny.eder@gmail.com>
2019-05-20 10:00:13 +0200

    Fixed generator

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -5,7 +5,12 @@ import "testing"
 func TestSudokuGeneratorBackTracking_Generate(t *testing.T) {
 	generator := NewBacktrackingGenerator()
 
-	sudoku := generator.Generate(0.1)
+	sudoku, err := generator.Generate(0.1)
+	if err != nil {
+		t.Errorf("expected no error")
+		return
+	}
+
 	if sudoku.IsComplete() {
 		t.Errorf("Sudoku is complete")
 	}
